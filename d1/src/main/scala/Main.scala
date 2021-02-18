7dce164c657832e03956439c51f5a58bb5d2a139
VAlux <olex3000sp@gmail.com>
2021-02-18 10:14:22 +0200

    small d1 refactoring

--- d1/src/main/scala/Main.scala
+++ d1/src/main/scala/Main.scala
@@ -1,15 +1,13 @@
-import scala.util.chaining._
-
-@main def hello: Unit =
+@main def entrypoint: Unit =
     println(run)
 
 def run = 
   val input = FileLoader.readFile("input.txt").map(_.toInt)
-  go(input.head, input.tail)
+  locate(input.head, input.tail)
 
-def go(elem: Int, rem: List[Int], target: Int = 2020): Int = rem match
+def locate(elem: Int, rem: List[Int], target: Int = 2020): Int = rem match
     case Nil => 0
     case _ => rem
         .filter(lo => lo + elem < target)
         .flatMap(lo => rem.find(hi => hi + lo + elem == target).map(hi => hi * lo * elem))
-        .headOption.getOrElse(go(rem.head, rem.tail))
+        .headOption.getOrElse(locate(rem.head, rem.tail))
