17479c6e1bc8f1fc6b6841f10de5ee4de527fda8
VAlux <olex3000sp@gmail.com>
2021-02-21 12:50:57 +0200

    day 2 part 1 finalized

--- d3/src/main/scala/MainD3.scala
+++ d3/src/main/scala/MainD3.scala
@@ -3,13 +3,15 @@
 
 def solve(input: List[String]): Int = 
   val rows = input.map(_.toCharArray)
-  go(rows(1), rows.drop(1))
+  go(rows(1), rows.drop(1).tail)
 
 def go(currentRow: Array[Char], rem: List[Array[Char]], column: Int = 3, trees: Int = 0): Int = rem match
-  case Nil => trees
-  case head :: tail if currentRow(column) == '#' => go(head, tail, calcColumn(column, currentRow.size), trees + 1)
+  case Nil => if isTree(currentRow, column) then trees + 1 else trees
+  case head :: tail if isTree(currentRow, column) => go(head, tail, calcColumn(column, currentRow.size), trees + 1)
   case _ => go(rem.head, rem.tail, calcColumn(column, currentRow.size), trees)
 
 def calcColumn(column: Int, rowLength: Int): Int = 
-  if column + 3 >= rowLength then 3 - (rowLength % column)
+  if column + 3 >= rowLength then  (column + 3) - rowLength
   else column + 3
+
+def isTree(currentRow: Array[Char], column: Int): Boolean = currentRow(column) == '#'
