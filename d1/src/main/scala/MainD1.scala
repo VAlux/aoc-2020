d7e0e753230bd6fd7321e41c92aa055816ee1dc2
VAlux <olex3000sp@gmail.com>
2021-02-25 23:57:30 +0200

    d1 v1 added, small refactoring

--- d1/src/main/scala/MainD1.scala
+++ d1/src/main/scala/MainD1.scala
@@ -5,9 +5,12 @@ def run =
   val input = FileLoader.readFile("input.txt").map(_.toInt)
   locate(input.head, input.tail)
 
+def locateV1(elem: Int, rem: List[Int], target: Int = 2020): Int =
+  rem.find(_ + elem == target).headOption.map(_ * elem).getOrElse(locateV1(rem.head, rem.tail))
+
 def locate(elem: Int, rem: List[Int], target: Int = 2020): Int = rem match
-    case Nil => 0
-    case _ => rem
-        .filter(lo => lo + elem < target)
-        .flatMap(lo => rem.find(hi => hi + lo + elem == target).map(hi => hi * lo * elem))
-        .headOption.getOrElse(locate(rem.head, rem.tail))
+  case Nil => 0
+  case _ => rem
+    .filter(lo => lo + elem < target)
+    .flatMap(lo => rem.find(hi => hi + lo + elem == target).map(hi => hi * lo * elem))
+    .headOption.getOrElse(locate(rem.head, rem.tail))
