ad245683f2463a3cd2d11a3a40b5ed6e6297b630
VAlux <olex3000sp@gmail.com>
2021-02-26 00:16:36 +0200

    day 1 optimized version

--- d1/src/main/scala/MainD1.scala
+++ d1/src/main/scala/MainD1.scala
@@ -1,9 +1,19 @@
+import scala.annotation.tailrec
+
 @main def entrypoint: Unit =
     println(run)
 
 def run = 
   val input = FileLoader.readFile("input.txt").map(_.toInt)
-  locate(input.head, input.tail)
+  // locate(input.head, input.tail)
+  locateOpt(input.sorted, 0, input.size - 1)
+
+@tailrec
+def locateOpt(elems: List[Int], lo: Int, hi: Int): Int =
+  val sum = elems(lo) + elems(hi)
+  if sum == 2020 then elems(lo) * elems(hi) 
+  else if sum < 2020 then locateOpt(elems, lo + 1, hi) 
+  else locateOpt(elems, lo, hi - 1)
 
 def locateV1(elem: Int, rem: List[Int], target: Int = 2020): Int =
   rem.find(_ + elem == target).headOption.map(_ * elem).getOrElse(locateV1(rem.head, rem.tail))
