ce62dead036fb39a60b21233d2b21d573f77061e
VAlux <olex3000sp@gmail.com>
2021-04-01 22:51:41 +0300

    dfs implementation

--- d7/src/main/scala/MainD7.scala
+++ d7/src/main/scala/MainD7.scala
@@ -2,20 +2,21 @@ import scala.util.chaining._
 
 case class GraphEdge(val weight: Int, val data: String)
 
-type Graph = Map[String, Set[GraphEdge]]
+type Graph = Map[String, List[GraphEdge]]
 
 @main def entrypoint = 
-  println(solve(FileLoader.readFile("input_test.txt")) mkString "\n")
+  println(solve(FileLoader.readFile("input_test.txt"), "shiny gold") mkString "\n")
 
 case class ContainerContents(amount: Int, color: String)
 case class ContainerDefinition(containerColor: String, contents: List[ContainerContents])
 
-def solve(input: List[String]) = 
+def solve(input: List[String], target: String) = 
   input
     .map(line => line.split("contain").toList)
     .map(_.map(_.replace("bags", "").replace("bag", "").replace(".", "").trim))
     .flatMap(parseContainerDefinitions)
     .pipe(buildGraph)
+    .pipe(graph => dfs(graph.removed(target), target))
 
 def parseContainerDefinitions(definitions: List[String]): Option[ContainerDefinition] =
   definitions match
@@ -28,7 +29,6 @@ def parseContainerContents(contents: String): List[ContainerContents] =
       case (amount, color) => 
         if color != "no other" then Some(ContainerContents(amount.toInt, color.trim))
         else None
-      case _ => None
   }
 
 def buildGraph(definitions: List[ContainerDefinition]): Graph = 
@@ -39,5 +39,12 @@ def buildGraph(definitions: List[ContainerDefinition]): Graph =
   
   go(definitions.head, definitions.tail)
 
-def generateEdges(definition: ContainerDefinition): (String, Set[GraphEdge]) = 
-  (definition.containerColor -> definition.contents.map(content => GraphEdge(content.amount, content.color)).toSet)
+def generateEdges(definition: ContainerDefinition): (String, List[GraphEdge]) = 
+  (definition.containerColor -> definition.contents.map(content => GraphEdge(content.amount, content.color)))
+
+def dfs(graph: Graph, targetData: String): List[List[GraphEdge]] = 
+  def trace(current: GraphEdge, path: List[GraphEdge] = List.empty): List[GraphEdge] = 
+    if current.data == targetData then path
+    else graph(current.data).filterNot(path.contains).flatMap(edge => trace(edge, path :+ current))
+
+  graph.values.flatMap(edges => edges.map(edge => trace(edge))).toList.filterNot(_.isEmpty)
