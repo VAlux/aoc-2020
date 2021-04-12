1e7385402014366b951ebc1d890c4c8752936f33
VAlux <olex3000sp@gmail.com>
2021-04-12 21:19:34 +0300

    d7 p2 some visual debug outputs

--- d7/src/main/scala/MainD7.scala
+++ d7/src/main/scala/MainD7.scala
@@ -2,6 +2,7 @@ import scala.util.chaining._
 import scala.annotation.tailrec
 
 type Graph[A] = Map[A, List[GraphEdge[A]]]
+type SGraph = Graph[String]
 
 case class GraphEdge[A](val weight: Int, val data: A)
 case class ContainerContents(amount: Int, color: String)
@@ -9,9 +10,11 @@ case class ContainerDefinition(containerColor: String, contents: List[ContainerC
 
 @main def entrypoint =
   val target = "shiny gold"
-  val tracedPaths = solve(FileLoader.readFile("input.txt"), target).distinctBy(path => path(0).data)
-  tracedPaths.foreach(path => println(showPath(path, target) + "\n\n"))
-  println(tracedPaths.size)
+  val tracedPaths = solve(FileLoader.readFile("input_test2.txt"), target)
+  // tracedPaths.foreach(path => println(showPathTo(path, target) + "\n\n"))
+  tracedPaths.foreach(path => println(showPathFrom(path) + "\n\n"))
+  val sum = tracedPaths.foldLeft(0L)((acc, path) => acc + weightSum(path))
+  println(sum)
 
 def solve(input: List[String], target: String) = 
   input
@@ -19,7 +22,8 @@ def solve(input: List[String], target: String) =
     .map(_.map(_.replace("bags", "").replace("bag", "").replace(".", "").trim))
     .flatMap(parseContainerDefinitions)
     .pipe(buildGraph)
-    .pipe(graph => dfs(graph.removed(target), target))
+    // .pipe(graph => dfs(graph.removed(target), target))
+    .pipe(graph => traceAllContents(graph, target))
 
 def parseContainerDefinitions(definitions: List[String]): Option[ContainerDefinition] =
   definitions match
@@ -34,9 +38,9 @@ def parseContainerContents(contents: String): List[ContainerContents] =
         else None
   }
 
-def buildGraph(definitions: List[ContainerDefinition]): Graph[String] = 
+def buildGraph(definitions: List[ContainerDefinition]): SGraph = 
   @tailrec
-  def go(definition: ContainerDefinition, rem: List[ContainerDefinition], graph: Graph[String] = Map.empty): Graph[String] = 
+  def go(definition: ContainerDefinition, rem: List[ContainerDefinition], graph: SGraph = Map.empty): SGraph = 
     rem match
       case Nil => graph + generateEdges(definition)
       case head :: tail => go(rem.head, rem.tail, graph + generateEdges(definition))
@@ -46,18 +50,34 @@ def buildGraph(definitions: List[ContainerDefinition]): Graph[String] =
 def generateEdges(definition: ContainerDefinition): (String, List[GraphEdge[String]]) = 
   (definition.containerColor -> definition.contents.map(content => GraphEdge(content.amount, content.color)))
 
-def dfs(graph: Graph[String], targetData: String): List[List[GraphEdge[String]]] = 
-  def trace(current: GraphEdge[String], path: List[GraphEdge[String]]): List[GraphEdge[String]] = 
+def dfs(graph: SGraph, targetData: String): List[List[GraphEdge[String]]] = 
+  def search(current: GraphEdge[String], path: List[GraphEdge[String]]): List[GraphEdge[String]] = 
     if current.data == targetData then path
-    else graph(current.data).filterNot(path.contains).flatMap(edge => trace(edge, path :+ current))
+    else graph(current.data).filterNot(path.contains).flatMap(edge => search(edge, path :+ current))
 
   graph
-    .flatMap { case (value, edges) => edges.map(edge => trace(edge, List(GraphEdge(edge.weight, value)))) }
+    .flatMap { case (value, edges) => edges.map(edge => search(edge, List(GraphEdge(edge.weight, value)))) }
     .toList
     .filter(_.size > 1)
 
-def showAsGraphVizNotation(graph: Graph[String]): String =
-  s"${graph.map { case (value, edges) => "\"" + value + "\"" + s" -> ${edges.map(edge => "\"" + edge.data + "\"") mkString ","};\n" }}"
+def traceAllContents(graph: SGraph, startFrom: String): List[List[GraphEdge[String]]] = 
+  def trace(current: GraphEdge[String], path: List[GraphEdge[String]]): List[GraphEdge[String]] = 
+    graph.get(current.data) match 
+      case Some(edges: List[GraphEdge[String]]) if !edges.isEmpty => 
+        edges.filterNot(path.contains).flatMap(edge => trace(edge, path :+ current))
+      case _ => 
+        path :+ current
 
-def showPath(path: List[GraphEdge[String]], target: String): String =
+  graph(startFrom).map(edge => trace(edge, List(GraphEdge(1, startFrom)))).toList
+
+def showPathTo(path: List[GraphEdge[String]], target: String): String =
  s"${path.map(_.data) mkString " -> "} -> $target"
+
+def showPathFrom(path: List[GraphEdge[String]]): String =
+  (path.sliding(2, 1).map {
+    case (edge1 :: edge2 :: Nil) => edge1.data + s" --[${edge2.weight}]--> " 
+    case _ => "###"
+  } mkString "") + path.last.data
+
+def weightSum(path: List[GraphEdge[String]]): Long = 
+  path.sliding(2, 1).foldLeft(0)((a, b) => a + b.map(_.weight).reduce(_ * _))
