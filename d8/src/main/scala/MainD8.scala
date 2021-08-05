82f9a01388352db0e0774815deb22d728246f3fd
VAlux <olex3000sp@gmail.com>
2021-08-05 15:01:28 +0300

    day 8 part 2 completed

--- d8/src/main/scala/MainD8.scala
+++ d8/src/main/scala/MainD8.scala
@@ -1,5 +1,3 @@
-import scala.annotation.tailrec
-
 case class Action(input: Int, term: Term)
 case class State(counter: Int, instructionPointer: Int)
 
@@ -9,13 +7,14 @@ enum Term(val action: Int => State => State):
   case JMP extends Term(value => state => state.copy(instructionPointer = state.instructionPointer + value))
 
 def run(actions: List[Action]): State = 
-  @tailrec
   def go(currentAction: Action, currentState: State, visited: Set[Int] = Set.empty): State =
-    println(s"$currentAction, $currentState")
     if visited.contains(currentState.instructionPointer) then currentState
     else 
       val newState = currentAction.term.action(currentAction.input)(currentState)
-      go(actions(newState.instructionPointer), newState, visited + currentState.instructionPointer)
+      actions
+        .lift(newState.instructionPointer)
+        .map(action => go(action, newState, visited + currentState.instructionPointer))
+        .getOrElse(currentState)
   
   go(actions.head, State(0, 0))
 
@@ -29,10 +28,20 @@ def parseTermsList(input: List[String]): List[Action] =
       case _ => None
   }
 
-def fixBrokenInstruction(actions: List[Action]): State = ???
+def fixBrokenInstruction(actions: List[Action]): Option[State] = 
+  def attempt(index: Int, value: Int, term: Term): Option[State] =
+    val attempt = run(actions.updated(index, Action(value, term)))
+    if attempt.instructionPointer >= actions.size - 1 then Some(attempt) else None
+
+  actions.zipWithIndex.flatMap { (action, index) => 
+    action match
+      case Action(value, Term.NOP) => attempt(index, value, Term.JMP)
+      case Action(value, Term.JMP) => attempt(index, value, Term.NOP)
+      case _ => None
+  }.headOption
 
 def solve(input: List[String]): Int =
-  run(parseTermsList(input)).counter
+  fixBrokenInstruction(parseTermsList(input)).map(_.counter).getOrElse(-1)
 
 @main def entrypoint =
   println(solve(FileLoader.readFile("input.txt")))
