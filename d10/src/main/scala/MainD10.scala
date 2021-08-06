4bdb5360d2d8c02db88159e40a5e3aef3fa608d8
VAlux <olex3000sp@gmail.com>
2021-08-06 12:19:31 +0300

    day 10 part 1 done

--- d10/src/main/scala/MainD10.scala
+++ d10/src/main/scala/MainD10.scala
@@ -1,4 +1,17 @@
-def solve(input: List[String]) = ???
+case class DiffSummary(singleJoltDiffsCount: Int, tripleJoltDiffsCount: Int)
+
+def calculateDiffSummary(input: List[String]) =
+  def diffList(list: List[Int]): Int =
+    list match
+      case a :: b :: Nil => b - a
+      case _             => -1
+
+  val adapters = input.map(_.toInt).sorted
+  val chain = 0 +: adapters :+ (adapters.last + 3)
+  val diffs = chain.sliding(2).map(diffList).toList
+
+  DiffSummary(diffs.count(_ == 1), diffs.count(_ == 3))
 
 @main def entrypoint =
-  println(solve(FileLoader.readFile("input.txt")))
+  val diff = calculateDiffSummary(FileLoader.readFile("input.txt"))
+  println(diff.singleJoltDiffsCount * diff.tripleJoltDiffsCount)
