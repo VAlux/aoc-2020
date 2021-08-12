ccd22f6cdc2d8bc6f1848e3abaa7db8ddfc50b92
VAlux <olex3000sp@gmail.com>
2021-08-12 17:28:20 +0300

    d10 p2 WIP

--- d10/src/main/scala/MainD10.scala
+++ d10/src/main/scala/MainD10.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 case class DiffSummary(singleJoltDiffsCount: Int, tripleJoltDiffsCount: Int)
 
 def calculateDiffSummary(input: List[String]) =
@@ -12,6 +14,34 @@ def calculateDiffSummary(input: List[String]) =
 
   DiffSummary(diffs.count(_ == 1), diffs.count(_ == 3))
 
+def trace(input: List[String]): List[List[Int]] =
+  def pass(current: Int, rem: List[Int]): List[Int] =
+    rem.filter(a => a - current == 1)
+      ++ rem.filter(a => a - current == 2)
+      ++ rem.filter(a => a - current == 3)
+
+  @tailrec
+  def go(current: List[Int], rem: List[Int], acc: List[List[Int]] = List.empty): List[List[Int]] =
+    rem match
+      case _ :: Nil => acc
+      case _ :: xs =>
+        val alternatives = current.flatMap(c => pass(c, xs))
+        if alternatives.nonEmpty then go(alternatives, xs, acc :+ alternatives)
+        else go(List(xs.head), xs.tail, acc :+ current)
+      case _ => acc
+
+  val adapters = input.map(_.toInt).sorted
+  val device = (adapters.last + 3)
+  val chain = 0 +: adapters :+ device
+
+  go(List(chain.head), chain.tail)
+
+def transpose(list: List[List[Int]]): List[List[Int]] = list.filter(_.nonEmpty) match
+  case Nil => Nil
+  case xs  => xs.map(_.head) :: transpose(xs.map(_.tail))
+
 @main def entrypoint =
-  val diff = calculateDiffSummary(FileLoader.readFile("input.txt"))
-  println(diff.singleJoltDiffsCount * diff.tripleJoltDiffsCount)
+  val input = FileLoader.readFile("input_test.txt")
+  // val diff = calculateDiffSummary(input)
+  // println(diff.singleJoltDiffsCount * diff.tripleJoltDiffsCount)
+  println(transpose(trace(input)) mkString "\n")
